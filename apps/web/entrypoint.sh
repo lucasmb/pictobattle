ef707e99b23626f30afecdd283aafc0d3a88c4bf
Lucas Borio <lucasborio@gmail.com>
2026-03-07 13:34:29 -0300

    imprived compose

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Syncing static files to shared volume..."
 # Copy all files from /app/dist (built-in) to /usr/share/caddy (volume mount)
-cp -ru /app/dist/. /usr/share/caddy/
+cp -ru /app/dist/. /usr/share/caddy/html
 
 echo "Static files synced. Keeping container alive..."
 exec sleep infinity
